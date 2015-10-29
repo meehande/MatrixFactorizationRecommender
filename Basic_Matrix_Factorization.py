379b42abe69d96d3e1905ac1bdcfaba7bdc7f22b
Deirdre Meehan <deirdremeehan16@gmail.com>
2015-10-29 20:47:09 +0000

    row by row basic matrix factorization

--- Basic_Matrix_Factorization.py
+++ Basic_Matrix_Factorization.py
@@ -5,7 +5,7 @@ R_matrix = numpy.matrix('1,2,;3,5')
 
 
 R_tilde = numpy.matrix('0,0 ; 0,0')
-iterations = 1000
+iterations = 5
 sigma_rat = 0
 rank = 2
 n = 2
@@ -14,9 +14,10 @@ d = 2
 sigma = 0
 
 #begin with random guesses of what could give R
-U_matrix = numpy.random.random_integers(1, 6, (2, 2))
+U_matrix = 5*numpy.random.rand(2,2)
 
-V_matrix = numpy.random.random_integers(1, 6, (2, 2))
+V_matrix = 5*numpy.random.rand(2,2)
+"""
 print "Umat"
 print U_matrix
 print "Vmat"
@@ -31,20 +32,32 @@ print "V_matrix[1, :].T"
 print V_matrix[1, :].T
 print "R_matrix[1, :]"
 print R_matrix[1, :]
+
+R_row = numpy.matrix('1,2,3')
+V_mat = numpy.matrix('1,2; 5,6;7,8')
+print R_row
+print V_mat
+print "dot"
+print numpy.dot(R_row, V_mat) # = R*V elementwise
 """
-for u in range (iterations):
-    # U_matrix = numpy.linalg.solve(numpy.dot(V_matrix,V_matrix.T)+ sigma_rat*numpy.eye(2), numpy.dot(V_matrix, R_matrix.T)).T
-    U_matrix[u,:] = numpy.linalg.solve(numpy.dot(V_matrix[:, u].T, V_matrix[:, u])+sigma_rat*numpy.eye(2), numpy.dot(V_matrix[u, :].T, R_matrix[u, :]))
-    #IS Ruv A SCALAR??
 
-for v in range(n): #for each row
-    #V_matrix = numpy.linalg.solve(numpy.dot(U_matrix.T,U_matrix)+ sigma_rat*numpy.eye(2), numpy.dot(U_matrix.T, R_matrix))
-    V_matrix[:,v] = numpy.linalg.solve(numpy.dot(U_matrix.T, U_matrix) + sigma_rat*numpy.eye(2), numpy.dot(U_matrix.T, R_matrix[:, v].T)).T
-
-R_tilde = numpy.dot(U_matrix.T, V_matrix)
+for i in range(iterations):
 
 
-print R_tilde"""
+    for u in range (m): # u = row
+        U_matrix[u, :] = numpy.dot(numpy.dot(R_matrix[u, :], V_matrix.T), numpy.linalg.pinv(numpy.asmatrix(numpy.dot(V_matrix, V_matrix.T))))
+        # U_matrix = numpy.linalg.solve(numpy.dot(V_matrix,V_matrix.T)+ sigma_rat*numpy.eye(2), numpy.dot(V_matrix, R_matrix.T)).T
+        # U_matrix[u,:] = numpy.linalg.solve(numpy.dot(V_matrix[:, u].T, V_matrix[:, u])+sigma_rat*numpy.eye(2), numpy.dot(V_matrix[u, :].T, R_matrix[u, :]))
+        #IS Ruv A SCALAR??
+
+    for v in range(n): #for each row
+        #V_matrix = numpy.linalg.solve(numpy.dot(U_matrix.T,U_matrix)+ sigma_rat*numpy.eye(2), numpy.dot(U_matrix.T, R_matrix))
+        # V_matrix[:,v] = numpy.linalg.solve(numpy.dot(U_matrix.T, U_matrix) + sigma_rat*numpy.eye(2), numpy.dot(U_matrix.T, R_matrix[:, v].T)).T
+        V_matrix[:, v] = numpy.dot(numpy.dot(R_matrix[:, v].T, U_matrix.T), numpy.linalg.pinv(numpy.asmatrix(numpy.dot(U_matrix, U_matrix.T))))
+
+    R_tilde = numpy.dot(U_matrix.T, V_matrix)
+
+    print R_tilde
 
 """
 for i in range (iterations):
