fc1f51c0a6ed3957a0b1e5f37d5a5e0ca2a6779a
Deirdre Meehan <deirdremeehan16@gmail.com>
2015-11-19 21:25:09 +0000

    RMSE edited to only look at non-zero elements from original matrix

--- rmse.py
+++ rmse.py
@@ -7,17 +7,17 @@ Created on Wed Nov 18 09:47:07 2015
 import math 
 import numpy
 
+#A is the original matrix (R_matrix) - need to test it for zero elements
 def rmse(A, B):
     # sqrt(sum(yn-y)^2)
     # element by element sum
-    
-    # 1. Element-wise subtraction
-    C = A - B
-    # 2. Square each element
-    numpy.square(C)
-    # 3. Sum of squares
-    # 4. Square root of answer
-    return math.sqrt(C.sum())
+     if(type(A) == numpy.ndarray):
+         # 1. Element-wise subtraction on non-zero elements!
+        C = A[A[:,:]!=0] - B[A[:,:]!=0]
+        # 2. Square each element
+        # 3. Sum of squares
+        # 4. Square root of answer
+        return math.sqrt(numpy.square(C).sum())
 
 #TO DO: ENSURE IT ONLY LOOKS AT NON-ZERO ELEMENTS! DON'T WANT TO INCLUDE RECOMMENDATIONS
 #TO DO: USE THIS FOR ITERATION CONTROL!
